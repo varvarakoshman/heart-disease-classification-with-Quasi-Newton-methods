b1e3f165c6be60eca90a9840af5b9b97e9f29a46
vkoschman <207955@niuitmo.ru>
2019-12-22 22:16:01 +0300

    some modifications to main

--- model.py
+++ model.py
@@ -2,13 +2,15 @@ import numpy as np
 import pandas as pd
 import scipy.optimize as sp
 from sklearn.model_selection import train_test_split
-
+from scipy.optimize import SR1
 from util import plot_cost_history
+from typing import Tuple
+from numpy import linalg
 
 # dataset and learning rate are available globally
 lr = 0.01
 epsilon = 0.0001
-data = pd.read_csv("../resources/heart.csv")
+data = pd.read_csv("resources/heart.csv")
 y_data = data.target.values
 x_data = data.drop(['target'], axis=1)
 x_data_norm = (x_data - np.min(x_data)) / (np.max(x_data) - np.min(x_data)).values
@@ -25,30 +27,35 @@ jac = lambda point: np.dot(x_train, (sigmoid(np.dot(point.T, x_train)) - y_train
 
 
 def main():
-    parameters_gd, iterations_gd = gradient_descent()
-    y_prediction = predict(np.array([q[0] for q in parameters_gd]))
-    print("Test Accuracy for GD: {:.2f}% with {} iterations".format(
-        (100 - np.mean(np.abs(y_prediction - y_test)) * 100), iterations_gd))
-    optimal_newton, newton_iterations = newton_method()
-    y_prediction = predict(optimal_newton)
-    print("Test Accuracy for Newton method: {:.2f}% with {} iterations".format(
-        (100 - np.mean(np.abs(y_prediction - y_test)) * 100), newton_iterations))
-    optimal_bfgs, bfgs_iterations = bfgs_method()
-    y_prediction = predict(optimal_bfgs)
-    print("Test Accuracy for BFGS method: {:.2f}% with {} iterations".format(
-        (100 - np.mean(np.abs(y_prediction - y_test)) * 100), bfgs_iterations))
-    optimal_lbfgs, lbfgs_iterations = lbfgs_method()
-    y_prediction = predict(optimal_lbfgs)
-    print("Test Accuracy for Limited memory BFGS method: {:.2f}% with {} iterations".format(
-        (100 - np.mean(np.abs(y_prediction - y_test)) * 100), lbfgs_iterations))
+    # parameters_gd, iterations_gd = gradient_descent()
+    # y_prediction = predict(np.array([q[0] for q in parameters_gd]))
+    # print("Test Accuracy for GD: {:.2f}% with {} iterations".format(
+    #     (100 - np.mean(np.abs(y_prediction - y_test)) * 100), iterations_gd))
 
+    # optimal_newton, newton_iterations = newton_method()
+    # y_prediction = predict(optimal_newton)
+    # print("Test Accuracy for Newton method: {:.2f}% with {} iterations".format(
+    #     (100 - np.mean(np.abs(y_prediction - y_test)) * 100), newton_iterations))
 
-def costF(param):
-    h = sigmoid(np.dot(param.T, x_train))
-    loss = -(y_train * np.log(h) + (1 - y_train) * np.log(1 - h))
-    cost = np.mean(loss)
-    gradient = np.dot(x_train, (h - y_train).T) / x_train.shape[1]
-    return cost, gradient
+    # optimal_bfgs, bfgs_iterations = bfgs_method()
+    # y_prediction = predict(optimal_bfgs)
+    # print("Test Accuracy for BFGS method: {:.2f}% with {} iterations".format(
+    #     (100 - np.mean(np.abs(y_prediction - y_test)) * 100), bfgs_iterations))
+
+    # optimal_lbfgs, lbfgs_iterations = lbfgs_method()
+    # y_prediction = predict(optimal_lbfgs)
+    # print("Test Accuracy for Limited memory BFGS method: {:.2f}% with {} iterations".format(
+    #     (100 - np.mean(np.abs(y_prediction - y_test)) * 100), lbfgs_iterations))
+
+    optimal_sr1, sr1_iterations = sr1_method()
+    y_prediction = predict(optimal_sr1)
+    print("Test Accuracy for SR1 method: {:.2f}% with {} iterations".format(
+        (100 - np.mean(np.abs(y_prediction - y_test)) * 100), sr1_iterations))
+
+    # optimal_bhhh, bhhh_iterations = bhhh_method()
+    # y_prediction = predict(optimal_bhhh)
+    # print("Test Accuracy for bhhh method: {:.2f}% with {} iterations".format(
+    #     (100 - np.mean(np.abs(y_prediction - y_test)) * 100), bhhh_iterations))
 
 
 def sigmoid(z):
@@ -72,7 +79,8 @@ def gradient_descent():
     iterations = 0
     cost_history = []
     while True:
-        cost, gradient = costF(prev)
+        cost = cost_function(prev)
+        gradient = np.dot(x_train, (sigmoid(np.dot(prev.T, x_train)) - y_train).T) / x_train.shape[1]
         curr = prev - lr * gradient
         iterations += 1
         cost_history.append(cost)
@@ -115,6 +123,35 @@ def lbfgs_method():
     optimal_lbfgs = lbfgs_res.x
     lbfgs_iterations = lbfgs_res.nit
     return optimal_lbfgs, lbfgs_iterations
+
+
+def sr1_method():
+    return None
+
+# TODO: поменять цикл с 5000 итерациями на цикл с точностью 10е-3
+# def simple(y, x):
+#     return lambda p: ((p[0] * x + p[1]) - y) ** 2
+#
+#
+# def bhhh_method():
+#     prev = np.random.rand(x_train.shape[0], 1)
+#     a = []
+#     # m = np.array([1, 1])  # SUPPOSE_MIN  np.array([1, 1])
+#     a.append(prev)
+#     iterations = 0
+#     while iterations <= 5000:
+#         r = np.zeros((2, 2))
+#         e = jac(prev)
+#         for x1, y1 in zip(x, y): # что с x, y
+#             j = jacobian(prev, simple(y1, x1)) # что за simple
+#             r += j.dot(j.T)
+#         prev = prev.reshape(2, 1) # что с размерностями
+#         prev = prev - 0.01 * linalg.inv(r).dot(e)
+#         prev = prev.reshape(2)
+#         a.append(prev)
+#         iterations += 1
+#     print(a[-1])
+#     return a
 
 
 if __name__ == '__main__':
